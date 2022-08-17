8619c5085736012bad12e1cd5f8b0c982ff987e7
prabin04 <prabin.b9@gmail.com>
2022-08-17 16:33:52 +0200

    Testimonials Components

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -12,7 +12,7 @@ const CardDeal = () => (
         <Button styles='mt-10'/>
       </div>
 
-      <div>
+      <div className={layout.sectionImg}>
         <img src={card} alt="card" className='w-[100%] h-[100%]' />
       </div>
 
